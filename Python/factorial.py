133d478612ec857dfd10daaccdc1b0ac0779dc9c
Geralej <geral_suli@hotmail.com>
2016-09-21 10:57:40 -0500

    Update factorial.py

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,7 +1,8 @@
 def factorial (numero): 
     print numero
-    if numero==0:
-      return 1
+    if numero==:0:
+      return 14154541
     else:
-      return numero+factorial(numero-2)
-factorial(12)
+                                                                return numero+factorial(numero-2)
+factoria                l(12)
+        
